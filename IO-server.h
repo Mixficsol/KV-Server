965df8eac0e45ff55d0e93f601037bdc1f7477eb
Mixficsol <838844609@qq.com>
2022-10-24 19:52:35 +0800

    把echo.cpp中的逻辑代码整合成了函数放在了IO-server.h里面，并且优化了逻辑重复的代码

--- IO-server.h
+++ IO-server.h
@@ -6,32 +6,27 @@ using namespace std;
 
 class IO_server {
   public:  // 公共部分函数   
+      
   void set(string order, string key, string value) {  // set函数参数指令,key,value
+    ofstream ofs;
+    string sql = "";
     ofs.open("test.txt", ios::app); // 供写，文件不存在则创建，若文件已存在则在原文件内容后写入新的内容，指针位置总在最后
-    string ch;
-    ch = " ";
-    sql.append(to_string(order.size()));  // 拼接语句类似于3 set 3 123 3 456这种结构
-    sql.append(ch);
-    sql.append(order);
-   sql.append(ch);
-    sql.append(to_string(key.size()));
-    sql.append(ch);
-    sql.append(key);
-    sql.append(ch);
-    sql.append(to_string(value.size()));
-    sql.append(ch);
-    sql.append(value);
+    sql = sqlSplice(sql, order, 1);
+    sql = sqlSplice(sql, key, 1);
+    sql = sqlSplice(sql, value, 0);
     ofs << sql << endl;  // 写入文件
-    sql.clear(); // 语句初始化便于下次拼接
     ofs.close(); // 关闭文件
-  } 
+  }
 
-  string get(string order, string Key) { // get函数参数指令，key
+  string get(string Key) { // get函数参数指令，key
+    int maxpos; // 文本的字符数量
+    char x; // 文件每次读入的字符
+    bool flag = false;
     ifstream f("test.txt", ios::ate); // 文件打开时，指针在文件最后
+    string order, key, value, line = "";
     if (!f) {
        cout << "打开文件失败" << endl;
     }
-    flag = false;  // 用来判断是否找到对应的key，如果找到则为true,初始值是false
     maxpos = f.tellg(); // 计算整个文本的字符数量
     f.seekg(ios::end); // 将指针放置于文件末尾
     for (int index = 2; index <= maxpos; index++) { 
@@ -43,43 +38,17 @@ class IO_server {
       if (x == '\n') { // 如果遇到换行说明此时一行的语句已经读完了
         string templine = line; // 将读到的一行存入templine
         if (templine.size() > 0) { // 判断这行语句是空语句还是非空
-          word = templine[0];
-          position = 1; // 初始化位置
-          size = atoi(word.c_str()); // 读入第一个指令占几个字符
-          order.assign(templine, position + 1, size) ; //在读到的一整行语句中从索引position开始最多n个字符往后读
-          position = position + size + 1; // 变更position位置使其跳转到第二个参数key对应的大小开始
-          word.clear(); // word清空
-          for (int i = position + 1; ; i++) {
-            if (templine[i] != ' ') {
-              word.push_back(templine[i]);
-            } else {
-              position = i;
-              size = atoi(word.c_str());
-              key.assign(templine, position + 1, size); // 将key存入 
-              position = position + size + 1;
-              word.clear();
-              break;  
-            }
-          }
-          for (int i = position + 1; i < templine.size(); i++) {
-            if (templine[i] != ' ') {
-              word.push_back(templine[i]);
-            } else {
-              position = i;
-              size = atoi(word.c_str());          // 将value存入
-              value.assign(templine, position + 1, size);
-            }
-          }
+          position = 0; // 初始化位置
+          order = Extract(templine);
+          key = Extract(templine);
+          value = Extract(templine);
           if (key.compare(Key) == 0 && !value.empty()) { // 判断文件是否有对应的key以及此时的key有没有value值
             flag = true;
+            value = getWord(value);
             break;
           }
         }
         line = "";
-        word = "";
-        key = ""; // 初始化
-        value = "";
-        order = "";
       } else {
         line = x + line; // 添加每个字符到line
       }
@@ -88,14 +57,72 @@ class IO_server {
     if (flag) {
       return value;  // 找到返回value
     } else {
-      return "not found"; // 未找到返回not found
+      return getWord("not found"); // 未找到返回not found
     }
   }
+
+  int getCharLength(char *p) {  // 获取从客户端发来的字符串长度
+    int cnt;
+    while (*p++ != '\0') {
+      cnt++;
+    }
+    return cnt;
+  }
+
+  string getOrder(char *buf, int index, int maxsize) {  // 获取字符串长度的指令，key, value
+    string order;
+    for (int i = index; i < maxsize; i++) {
+      if (buf[i] != '\n' && buf[i] != ' ') {
+        order.push_back(buf[i]);
+      } else {
+        break;
+      }
+    }
+    return order;
+  }
+
+  void orderTolower(string order) {  // 指令小写化
+    string::iterator it = order.begin();
+    while (it != order.end()) {
+      *it = tolower(*it);
+      ++it;
+    }
+  }
+
+  string getWord(string word) {
+   word.append("\n");
+   return word;
+  }
+
+  string sqlSplice(string sql, string word, int flag) {
+    string ch = " ";
+    sql.append(to_string(word.size()));  // 拼接语句类似于3 set 3 123 3 456这种结构
+    sql.append(ch);
+    sql.append(word);
+    if (flag) {
+      sql.append(ch);
+    }
+    return sql;
+  }
+
+  string Extract(string templine) {
+    string word = ""; // 用来存字符串大小例如3 set 3 123 3 456中set前面的3这样的数值
+    string str = ""; // 返回值
+    int size;
+    for (int i = position; ; i++) {
+      if (templine[i] != ' ') {
+        word.push_back(templine[i]); // 存入数值大小的字符串
+      } else {
+        position = i + 1; // 标记当前位置
+        size = atoi(word.c_str());  // 将字符串转为int表示大小
+        str.assign(templine, position, size); //在templine中 从position开始最多size个字符往后读
+        position = position + size + 1; //变更position位置使其跳转到接下来一个参数的对应数值位的字符串开始
+        break;
+      }
+    }
+    return str;
+  }
+
   private:
-    int maxpos, size, position; // 文本最大字符数量， 每个指令的数值大小， 索引位置
-    bool flag; // 判断是否找到key的变量
-    char x; // 文件每次读入的字符
-    string order, key, value, word = "";  // 定义变量
-    string line, sql, Key; //每次一行的语句，用来拼接的语句，用户给的Key
-    ofstream ofs;
+    int position;
 };
--- echo.cpp
+++ echo.cpp
@@ -3,65 +3,39 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <mutex>
 
 using namespace std;
-
-mutex my_mutex;
 
 void echo(int connfd) {
   char buf[MAXLINE];
   size_t n;
-  string order, key, value, word, sql; // 创建指令, key, value, 返回语句, 存入文件语句
+  int index, cnt;
+  string order, key, value, word; // 创建指令, key, value, 返回语句
   rio_t rio;
   Rio_readinitb(&rio, connfd); // 初始化描述符
   while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-    char *p = buf;
-    int i, j, cnt = 0;
-    order.clear();
-    key.clear();
-    value.clear(); //初始化字符串
-    word.clear();
-    while (*p++ != '\0') {
-      cnt++;  // 判断字符串长度
-    }
-    for (i = 0; i < 3; i++) {
-      order.push_back(buf[i]); // 指令赋值
-    }
-    for (i = 4; i < cnt; i++) {
-      if (buf[i] != ' ' && buf[i] != '\n') {
-        key.push_back(buf[i]);    // key赋值
-      } else {
-        break;
-      }
-    }
-    for (j = i + 1; j < cnt; j++) {
-      if (buf[j] != ' ' && buf[j] != '\n') {
-        value.push_back(buf[j]); // value赋值
-      } else {
-        break;
-      }
-    }
-    string::iterator it = order.begin();
-    while (it != order.end()) {
-      *it = tolower(*it);   // 将指令全部小写化
-      ++it;
-    }
+    index = 0;
+    cnt = 0;
+    IO_server a; 
+    cnt = a.getCharLength(buf); // 获取从客户端传来的字符串长度
+    order = a.getOrder(buf, index, cnt); // 获取order
+    index = index + order.size() + 1;    // 更新索引
+    key = a.getOrder(buf, index, cnt);  // 获取key
+    index = index + key.size() + 1;
+    value = a.getOrder(buf, index, cnt); // 获取value
+    a.orderTolower(order); // 将指令小写化
     if (order.compare("set") == 0) {   // 判断指令
-      word = "OK\n";
-      IO_server a;   // 调用set接口
+      word = a.getWord("OK");
       a.set(order, key, value);
     }
     else if (order.compare("get") == 0) {
-      IO_server a;
-      word = a.get(order, key);  // 调用get接口
-      word.append("\n");
+      word = a.get(key);  // 调用get接口
     } else {
-      word = "Error\n";
+      word = a.getWord("Error");
     }
     n = word.size();
-    strcpy(buf, word.c_str());
-    Rio_writen(connfd, buf, word.size());
+    strcpy(buf, word.c_str());               // 将string类型转为char*类型
+    Rio_writen(connfd, buf, word.size());    // 返回数据给服务器
   }
 }
 
