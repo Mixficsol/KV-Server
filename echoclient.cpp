69b4f08f6ae9a102eb8737aabd341b3feb077bb6
Mixficsol <838844609@qq.com>
2022-10-27 19:41:15 +0800

    使用了redis的客户端，在redis-3.0.2/src/redis-cli.c这个文件里面 ” git add . make exit

--- echoclient.cpp
+++ echoclient.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+//#include <iostream>
 #include "csapp.h"
-#include "Encode.h"
+//#include "Encode.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
   int clientfd, port, n;
-  string str;
+ // string str;
   char *host, buf[MAXLINE];
   rio_t rio;
 
@@ -20,16 +20,16 @@ int main(int argc, char **argv) {
   Rio_readinitb(&rio, clientfd);
 
   while (Fgets(buf, MAXLINE, stdin) != NULL) {
-    EncodeFix b;
+  //  EncodeFix b;
     Rio_writen(clientfd, buf, strlen(buf));
 	if ((n = Rio_readlineb(&rio, buf, MAXLINE) < 0)) {
       fprintf(stderr, "rio_readlineb error \n");        
     }
-    str = b.getOrder(buf, 0, 4);
+/*    str = b.getOrder(buf, 0, 4);
     if (str.compare("exit") == 0) {
       cout << "str: " << str << endl;
       break;
-    }
+    }*/
 	Fputs(buf, stdout);
   }
   Close(clientfd);
