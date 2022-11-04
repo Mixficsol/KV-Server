1e9e46d6dc1c133e48d1e205acdd0299970977b0
Mixficsol <838844609@qq.com>
2022-11-04 17:14:13 +0800

    完善了Encode.cpp对字符串的判断，使其遇到各种突发情况都能有解决方案

--- Encode.cpp
+++ Encode.cpp
@@ -2,55 +2,93 @@
 #include <vector>
 #include <string>
 #include <assert.h>
+#include <stdio.h>
 
 using namespace std;
 
-string a = "*3\r\n$3\r\nset\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
-int length = a.size();
 int cur_pos;
+int size, length;
 
-int FindNextSeparators() {
-  if (cur_pos >= length - 1) {
-    return -1;
+bool FindNextSeparators(const string& in) {  // 找到下一个'\n'的位置
+  if (cur_pos > length - 1) {
+    return false;
   }
   int pos = cur_pos;
-  while (pos < length - 1) {
-    if (a[pos] == '\n') {
-      return pos;
+  while (pos <= length - 1) {
+    if (in[pos] == 'n') {
+      cur_pos = pos + 1;  // 找到'\n'的索引并且返回, 使位置往后移一位
+      return true; 
     }
     pos++;
   }
-  return -1;
+  return false;
 } 
+
+
+void Split(const string& in, std::vector<std::string>* out) { // 使key, value, 指令装到vector里面
+  string str = "";
+  str.assign(in, cur_pos, size);
+  out->push_back(str);
+  cur_pos = cur_pos + size;
+}
+
+bool Judgestring(const string& in) {  // 判断是不是以*开头的序列化语句
+  if (in[cur_pos] == '*') {
+    cur_pos++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool JudgeOrder(const string& in) { // 判断是不是以$开头的参数语句
+  if (in[cur_pos] == '$') {
+    cur_pos++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool paramtertotal(const string& in) { // 使字符串指令变成int类型的size
+  string word = "";
+  string str = "";
+  int pos = cur_pos;
+  while (in[pos] != '\\') {
+    if (in[pos] >= 48 && in[pos] <= 57) { // 判断参数里面有没有掺杂字母比如3f4这种就不合理
+      word.push_back(in[pos]);
+      pos++;
+    } else {
+      cout << "false" << endl;
+      return false;  
+    }
+  }
+  size = atoi(word.c_str()); // 字符串转为int类型
+  cur_pos = pos; // 更新位置
+  return true;
+}
 
 int main() {
   vector<string> v;
-  int pos = cur_pos = 0;
-  int cnt = 3;
-  cout << "length: " << length << endl;
+  char str[1010] = {};
+  string in;
+  int cnt;
+  cur_pos = 0;
+  scanf("%s", &str);
+  cout << str << endl;
+  in = (string)str;
+  length = in.size();
+  assert(Judgestring(in));
+  assert(paramtertotal(in));
+  cnt = size;
   while (cnt--) {
-    pos = FindNextSeparators();
-    cout << "pos: " << pos << endl;
-    string word = "";
-    string str = "";
-    pos = pos + 1; // 使位置移到$处
-    if (a[pos] == '$') {
-      pos++;
-      while (a[pos] != '\r') {
-        word.push_back(a[pos]);
-        pos++;
-      }
-      cout << "word: " << word << endl;
-      int size = atoi(word.c_str());
-      cout << "size: " << size << endl;
-      str.assign(a, pos + 2, size);
-      cur_pos = pos + 2;
-      cout << "str: " << str << endl;
-      v.push_back(str);
-    }
+    assert(FindNextSeparators(in));
+    assert(JudgeOrder(in));
+    assert(paramtertotal(in));
+    assert(FindNextSeparators(in));
+    Split(in, &v);
   }
-  cout << v[0] << ' ' << v[1] << ' ' << v[2]  << endl;
-  
+  cout << v[0] << ' ' << v[1] << ' ' << v[2] << endl;
   return 0;
 }
 
---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+
+using namespace std;
+
+char str[1010] = {};
+
+int main() {
+  scanf("%s", &str);
+  int i = 0;
+  assert(i > 1);
+  cout << str << endl;
+  return 0;
+}
